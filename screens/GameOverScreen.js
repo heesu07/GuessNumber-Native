16c0185b59922fc5618bf6d5365febcdab2e3d96
Daniel <heesu07@gmail.com>
2021-04-16 21:51:26 +1200

    add constants-color,default

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
 import Card from '../components/Card';
 import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
-
+import Colors from '../constants/color';
 
 const GameOverScreen = (props) => {
   return(
     
       <Card style={styles.screen}>        
         <TitleText > Game Over!</TitleText>
-        <BodyText> Number of Round: {props.round} </BodyText>
-        <BodyText> Answer was {props.answer}</BodyText>
+        <View style={styles.imageContainer}>
+          <Image 
+            fadeDuration={1000}
+            style={styles.image} 
+            source={ require('../assets/images/success.png')} 
+            //source={{ uri: 'https://image.shutterstock.com/image-photo/mountaineers-help-each-other-reach-260nw-1340104646.jpg' }} 
+            resizeMethod= 'auto'
+          />
+        </View>
+        <BodyText> Number of Round: <Text style={styles.highlight}>{props.round} </Text> </BodyText>
+        <BodyText> Answer was <Text style={styles.highlight}>{props.answer}</Text></BodyText>
         <Button title='NEW GAME' onPress={props.startAgain}/>  
       </Card>
     
@@ -23,6 +32,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',    
+  },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: '#f7287b',
+    width: 300,
+    height: 300, 
+    overflow: 'hidden',
+    margin: 10,
+  },
+  highlight: {
+    color: Colors.primary,
   }
 })
 export default GameOverScreen;
